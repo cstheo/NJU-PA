f9560235221df016c086784397d7ea92fc0106ea
Anekoique <ctolu01@gmail.com>
2025-09-10 15:43:38 +0800

    feat(pa): Impl Klib Stdio and String

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,135 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+static void reverse(char *str, int len) {
+  char *start = str;
+  char *end = str + len - 1;
+  while (start < end) {
+    char temp = *start;
+    *start++ = *end;
+    *end-- = temp;
+  }
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+static int itoa(long long num, char *str, int base) {
+  int i = 0;
+  int neg = 0;
+  
+  if (num < 0 && base == 10) {
+    neg = 1;
+    num = -num;
+  }
+  
+  if (num == 0) {
+    str[i++] = '0';
+  } else {
+    while (num) {
+      int rem = num % base;
+      str[i++] = (rem > 9) ? (rem - 10 + 'a') : (rem + '0');
+      num /= base;
+    }
+  }
+  
+  if (neg) str[i++] = '-';
+  
+  reverse(str, i);
+  str[i] = '\0';
+  return i;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int written = 0;
+  const char *p = fmt;
+    
+  while (*p && written < n - 1) {
+    if (*p != '%') {
+      out[written++] = *p++;
+      continue;
+    }
+        
+    p++;
+        
+    switch (*p) {
+      case 'd': {
+        int val = va_arg(ap, int);
+        char temp[20];
+        int len = itoa(val, temp, 10);
+        for (int i = 0; i < len && written < n - 1; i++) {
+          out[written++] = temp[i];
+        }
+        break;
+      }
+      case 'x': {
+        unsigned int val = va_arg(ap, unsigned int);
+        char temp[20];
+        int len = itoa(val, temp, 16);
+        for (int i = 0; i < len && written < n - 1; i++) {
+          out[written++] = temp[i];
+        }
+        break;
+      }
+      case 's': {
+        char *str = va_arg(ap, char *);
+        if (!str) str = "(null)";
+        while (*str && written < n - 1) {
+          out[written++] = *str++;
+        }
+        break;
+      }
+      case 'c': {
+        char c = (char)va_arg(ap, int);
+        out[written++] = c;
+        break;
+      }
+      case '%': {
+        out[written++] = '%';
+        break;
+      }
+      default: {
+        out[written++] = '%';
+        if (written < n - 1) out[written++] = *p;
+        break;
+      }
+    }
+    p++;
+  }
+    
+  out[written] = '\0';
+  return written;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  return vsnprintf(out, 0x7FFFFFFF, fmt, ap);
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int result = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return result;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int result = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return result;
+}
+
+int printf(const char *fmt, ...) {
+  char buffer[1024];
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vsnprintf(buffer, sizeof(buffer), fmt, ap);
+  va_end(ap);
+  
+  for (int i = 0; i < len; i++) {
+    putch(buffer[i]);
+  }
+  
+  return len;
 }
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,82 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+	const char *a = s;
+	for (; *s; s++);
+	return s-a;
+}
+
+size_t strnlen(const char *s, size_t n) {
+  const char *a = s;
+  for (; n > 0 && *s; s++, n--);
+  return s - a;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *saved = dst;
+  while ((*dst++ = *src++));
+  return saved;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t size = strnlen(src, n);
+  if (size != n) 
+    memset(dst + size, '\0', n - size);
+  return memcpy(dst, src, size);
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  strcpy(dst + strlen(dst), src);
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+	for (; *s1==*s2 && *s1; s1++, s2++);
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+int strncmp(const char *_l, const char *_r, size_t n) {
+	const unsigned char *l=(void *)_l, *r=(void *)_r;
+	if (!n--) return 0;
+	for (; *l && *r && n && *l == *r ; l++, r++, n--);
+	return *l - *r;
 }
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+void *memset(void *dst, int c, size_t n) {
+	unsigned char *s = dst;
+	for (; n; n--, s++) *s = c;
+  return dst;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+	char *d = dst;
+	const char *s = src;
+
+	if (d==s) return d;
+	if ((uintptr_t)s-(uintptr_t)d-n <= -2*n) return memcpy(d, s, n);
+
+	if (d<s) {
+		for (; n; n--) *d++ = *s++;
+	} else {
+		while (n) n--, d[n] = s[n];
+	}
+
+	return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char *d = out;
+  const char *s = in;
+  for (; n; n--) *d++ = *s++;
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *p1 = s1, *p2 = s2;
+  for (; n--; p1++, p2++)
+    if (*p1 != *p2)
+       return *p1 - *p2;
+  return 0;
 }
 
 #endif
